68fc3054baf0fdc3fc32495670d7a9453e2c5fc3
workspace <13564472805@163.com>
2019-02-12 13:53:28 +0800

    array practice

--- java/src/array/MajorityElement.java
+++ java/src/array/MajorityElement.java
@@ -38,9 +38,61 @@ public class MajorityElement {
         return -1;
     }
 
+    /**
+     * Boyer-Moore Voting Algorithm (摩尔投票算法)：
+     *
+     * wikipedia :
+     *   The Boyer–Moore majority vote algorithm is an algorithm for finding the majority of a sequence of elements
+     * using linear time and constant space.In its simplest form, the algorithm finds a majority element, if there is
+     * one: that is, an element that occurs repeatedly for more than half of the elements of the input. However, if
+     * there is no majority, the algorithm will not detect that fact, and will still output one of the elements.
+     * A version of the algorithm that makes a second pass through the data can be used to verify that the element
+     * found in the first pass really is a majority.
+     *
+     * step :
+     *  1.initialize the element m and a counter i with i = 0
+     *  2.for each element x of the input sequence:
+     *    1) if i == 0, then assign m = x and i = 1
+     *    2) else if m == x,the assign i = i + 1
+     *    3) else assign i = i - 1
+     *  3.return m
+     *
+     * @param nums
+     * @return
+     */
+    public int majorityElement2(int[] nums){
+        int len = nums.length;
+
+        if(len == 0) return -1;
+        int result = nums[0];
+        int c = 1;
+        for(int i = 1; i < nums.length; i ++){
+            if(c == 0){
+                c = 1;
+                result = nums[i];
+            }else if(nums[i] == result) {
+                c++;
+            }else{
+                c --;
+            }
+        }
+
+        c = 0;
+        for(int v : nums){
+            if(v == result)
+                c ++;
+        }
+
+        if(c > len / 2)
+            return result;
+        return -1;
+    }
+
     public static void main(String[] args){
         MajorityElement m = new MajorityElement();
         System.out.println(m.majorityElement(new int[]{3,2,3}));
         System.out.println(m.majorityElement(new int[]{2,2,1,1,1,2,2}));
+        System.out.println(m.majorityElement2(new int[]{3,2,3}));
+        System.out.println(m.majorityElement2(new int[]{2,2,1,1,1,2,2}));
     }
 }
