04a4e63db74809b67a71a91d818bda36551030db
workspace <13564472805@163.com>
2019-07-26 17:06:41 +0800

    leetcode 140

--- java/src/dynamicprogramming/WordBreakII140.java
+++ java/src/dynamicprogramming/WordBreakII140.java
@@ -42,13 +42,17 @@ public class WordBreakII140 {
 	List<String> result;
 
 	/**
-	 * dfs, Time Limit Exceeded
+	 * way 1: dfs, Time Limit Exceeded
+	 * wordBreak_dfs3针对此方法进行优化
 	 */
 	public List<String> wordBreak_dfs(String s, List<String> wordDict) {
 		int n = s.length();
+
+		// contains[i]记录从当前位置i到达下一个位置，需要填充wordDict中字符串长度，可以有多个，若不存在则为null
 		List<Integer>[] contains = new ArrayList[n];
 
 		Set<String> dict = getDict(wordDict);
+		// [i,j] 左右全包括
 		for(int i = 0; i < n; i ++) {
 			for (int j = i; j < n; j ++) {
 				if(isContains(s, i, j, dict))
@@ -103,7 +107,7 @@ public class WordBreakII140 {
 
 
 	/**
-	 * dfs, (from network)
+	 * way 2: dfs, (from network)
 	 */
 	HashMap<Integer, List<String>> map;
 	public List<String> wordBreak_dfs2(String s, List<String> wordDict) {
@@ -131,7 +135,7 @@ public class WordBreakII140 {
 
 
 	/***
-	 * 动态规划, (from network)
+	 * way 3: dfs, (from network)
 	 */
 	public List<String> wordBreak_dp(String s, List<String> wordDict) {
 		// 长度为n的字符串有n+1个隔板
@@ -165,10 +169,68 @@ public class WordBreakII140 {
 			result.add(sb.toString());
 		}
 		for (int i = 0; i < s.length(); ++i) {
-			if (prev[cur][i]) {
+			if (prev[cur][i]) {   // 去除无效递归
 				path.add(s.substring(i, cur));
 				gen_path(s, prev, i, path, result);
 				path.remove(path.size()-1);
+			}
+		}
+	}
+
+
+	/**
+	 * way 4: 根据 wordBreak_dp 对超时递归 wordBreak_dfs 进行改动
+	 * 去除不能匹配到最后一个字符的无效递归，即递归生成路径时从最后一个字符下标开始，同时记录信息需要做相应的改动
+	 */
+	public List<String> wordBreak_dfs3(String s, List<String> wordDict) {
+		int n = s.length();
+
+		// contains[j]记录从前一个起始位置到位置j，需要填充wordDict中字符串长度，可以有多个，若不存在则为null
+		List<Integer>[] contains = new ArrayList[n];
+
+		Set<String> dict = getDict(wordDict);
+
+		boolean[] canStart = new boolean[n + 1];  // 表示当前位置是否可以作为起点，用于减少无效记录数
+		canStart[0] = true;
+		// [i,j] 左右全包括
+		for(int i = 0; i < n; i ++) {
+			for (int j = i; j < n; j ++) {
+				if(canStart[i] && isContains(s, i, j, dict)) {
+					canStart[j + 1] = true;
+					if (contains[j] == null) {
+						List<Integer> temp = new ArrayList<>();
+						temp.add(j - i + 1);
+						contains[j] = temp;
+					} else {
+						contains[j].add(j - i + 1);
+					}
+				}
+			}
+		}
+
+		result = new ArrayList<>();
+		dfs3(s, n - 1, n, contains, new ArrayList<>());
+		return result;
+	}
+
+	private void dfs3(String str, int s, int n, List<Integer>[] contains, List<String> list) {
+		if(s < -1) return;
+
+		if(s == -1) {
+			StringBuilder builder = new StringBuilder();
+			for(int i = list.size() - 1; i >= 0; i --) {
+				builder.append(list.get(i)).append(" ");
+			}
+			builder.deleteCharAt(builder.length() - 1);
+			result.add(builder.toString());
+			return;
+		}
+
+		if(contains[s] != null) {
+			for(int l : contains[s]) {
+				list.add(str.substring(s - l + 1, s + 1));
+				dfs3(str, s - l, n, contains, list);
+				list.remove(list.size() - 1);
 			}
 		}
 	}
@@ -187,6 +249,15 @@ public class WordBreakII140 {
 		System.out.println(w.wordBreak_dfs2("catsanddog", Arrays.asList("cat", "cats", "and", "sand", "dog")));
 		System.out.println(w.wordBreak_dfs2("pineapplepenapple", Arrays.asList("apple", "pen", "applepen", "pine", "pineapple")));
 		System.out.println(w.wordBreak_dfs2("catsandog", Arrays.asList("cats", "dog", "sand", "and", "cat")));
+
+		System.out.println(w.wordBreak_dp("catsanddog", Arrays.asList("cat", "cats", "and", "sand", "dog")));
+		
+		System.out.println(w.wordBreak_dfs3("catsanddog", Arrays.asList("cat", "cats", "and", "sand", "dog")));
+		System.out.println(w.wordBreak_dfs3("pineapplepenapple", Arrays.asList("apple", "pen", "applepen", "pine", "pineapple")));
+		System.out.println(w.wordBreak_dfs3("catsandog", Arrays.asList("cats", "dog", "sand", "and", "cat")));
+
+		System.out.println(w.wordBreak_dfs3("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+				Arrays.asList("a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa")));
 
 	}
 }
