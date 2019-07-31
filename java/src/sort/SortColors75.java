f21c05c2c4f86faaa77909d46a8280a9e62dd3dc
workspace <13564472805@163.com>
2019-07-31 17:28:41 +0800

    leetcode 75,324

---
+++ java/src/sort/SortColors75.java
@@ -0,0 +1,58 @@
+package sort;
+
+import java.util.Arrays;
+
+/**
+ * 75. Sort Colors
+ *   Given an array with n objects colored red, white or blue, sort them in-place so that objects of the same color are
+ * adjacent, with the colors in the order red, white and blue.
+ *
+ * Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+ *
+ * Note: You are not suppose to use the library's sort function for this problem.
+ *
+ * Example:
+ *   Input: [2,0,2,1,1,0]
+ *   Output: [0,0,1,1,2,2]
+ *
+ * Follow up:
+ *   A rather straight forward solution is a two-pass algorithm using counting sort.
+ *
+ *   First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's,
+ * then 1's and followed by 2's.
+ *
+ *   Could you come up with a one-pass algorithm using only constant space?
+ */
+public class SortColors75 {
+	/**
+	 * 3-way-partitioning: 将数组划分成3部分 A1,A2,A3；当 a1∈ A1, a2∈A2, a3∈A3 时，a1 < a2 < a3
+	 *
+	 * two point: p1,p2
+	 *
+	 * 时间复杂度： O(n); 空间复杂度： O(1)
+	 */
+	public static void sortColors(int[] nums) {
+		int n = nums.length;
+		if(n <= 1) return;
+
+		int p1 = 0, p2 = n - 1;
+		for(int i = 0; i <= p2;) {
+			if(nums[i] == 0) {
+				Utils.swap(nums, i, p1);
+				p1 ++;
+				i ++;
+			} else if(nums[i] == 2) {
+				Utils.swap(nums, i, p2);
+				p2 --;
+			} else {
+				i ++;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] nums = new int[] {2,0,2,1,1,0};
+		sortColors(nums);
+		System.out.println(Arrays.toString(nums));
+	}
+}
--- java/src/sort/WiggleSortII324.java
+++ java/src/sort/WiggleSortII324.java
@@ -38,13 +38,73 @@ public class WiggleSortII324 {
 	}
 
 	/**
-	 * TODO: 时间复杂度： O(n)
+	 * 时间复杂度： O(n)
+	 * step 1: 求解中位数，并获得 Dutch National Flag (DNF) 排序结果
+	 * step 2: 根据 Dutch National Flag (DNF) 得到结果
 	 */
 	public static void wiggleSortOptimize(int[] nums) {
 		int n = nums.length;
 		if(n <= 1) return;
 
+		int middle = median(nums);
 
+		/**                        L  L  L  M  M  S  S
+		 *  计算完中位数的 nums下标  0  1  2  3  4  5  6
+		 *  符合要求的结果          3  0  4  1  5  2  6
+		 *  相当于遍历原数组下标顺序为   1 3 5 0 2 4 6
+		 *
+		 */
+		int i = 0, j = n - 1;
+		for(int k = 0; k <= j;) {
+			if(nums[index(n, k)] > middle) {
+				Utils.swap(nums, index(n, k ++), index(n, i ++));
+			} else if(nums[index(n, k)] < middle) {
+				Utils.swap(nums, index(n, k), index(n, j --));
+			} else {
+				k ++;
+			}
+		}
+	}
+
+	private static int median(int[] nums) {
+		int n = nums.length;
+
+		int k = (n - 1) / 2 + 1; // 第k个最小数即为中位数
+
+		return partitioning(nums, 0, n - 1, k);
+	}
+
+	private static int index(int n, int i) {
+		// 若 n 为偶数: n = 6,  1 3 5 0 2 4       idx = (2*i+1)%(n+1)
+		// 若 n 为奇数：n = 7,  1 3 5 0 2 4 6     idx = (2*i+1)%n
+		return (2 * i + 1) % (n | 1);
+	}
+
+	private static int partitioning(int[] nums, int l, int r, int count) {
+		int pivot = nums[l];
+
+		// 3-way-partition
+		int k = l, i = l, j = r;
+		while(k <= j) {
+			if (nums[k] < pivot) {
+				Utils.swap(nums, k, j --);
+			} else if (nums[k] > pivot) {
+				Utils.swap(nums, i ++, k ++);
+			} else {
+				k++;
+			}
+		}
+
+		int c = i - l + 1;
+
+		if(count == c) {
+			return pivot;
+		} else	if(count > c) {
+			return partitioning(nums, i + 1, r, count - c );
+		}
+		else {
+			return partitioning(nums, l, i - 1, count);
+		}
 	}
 
 	public static void main(String[] args){
@@ -66,6 +126,27 @@ public class WiggleSortII324 {
 
 		nums = new int[]{1, 5, 1, 4, 2};
 		wiggleSort(nums);
+		System.out.println(Arrays.toString(nums));
+
+
+		nums = new int[]{1, 5, 1, 1, 6, 4};
+		wiggleSortOptimize(nums);
+		System.out.println(Arrays.toString(nums));
+
+		nums = new int[] {1, 3, 2, 2, 3, 1};
+		wiggleSortOptimize(nums);
+		System.out.println(Arrays.toString(nums));
+
+		nums = new int[] {2, 1};
+		wiggleSortOptimize(nums);
+		System.out.println(Arrays.toString(nums));
+
+		nums = new int[] {4, 5, 5, 6};
+		wiggleSortOptimize(nums);
+		System.out.println(Arrays.toString(nums));
+
+		nums = new int[]{1, 5, 1, 4, 2};
+		wiggleSortOptimize(nums);
 		System.out.println(Arrays.toString(nums));
 	}
 }
