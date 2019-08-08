4a3a067a3e6400668a41ac51e83a80d019e7e32c
workspace <13564472805@163.com>
2019-08-08 17:39:01 +0800

    leetcode 130,523,560

---
+++ java/src/array/ContinuousSubarraySum523.java
@@ -0,0 +1,44 @@
+package array;
+
+/**
+ * leetcode 523. Continuous Subarray Sum
+ *   Given a list of non-negative numbers and a target integer k, write a function to check if the array has a continuous
+ * subarray of size at least 2 that sums up to a multiple of k, that is, sums up to n*k where n is also an integer.
+ *
+ * Example 1:
+ *   Input: [23, 2, 4, 6, 7],  k=6
+ *   Output: True
+ *   Explanation: Because [2, 4] is a continuous subarray of size 2 and sums up to 6.
+ * Example 2:
+ *   Input: [23, 2, 6, 4, 7],  k=6
+ *   Output: True
+ *   Explanation: Because [23, 2, 6, 4, 7] is an continuous subarray of size 5 and sums up to 42.
+ * Note:
+ *   The length of the array won't exceed 10,000.
+ *   You may assume the sum of all the numbers is in the range of a signed 32-bit integer.
+ */
+public class ContinuousSubarraySum523 {
+	/**
+	 * 同 SubarraySumEqualsK560  way1
+	 * 时间复杂度：O(n^2)
+	 */
+	public static boolean checkSubarraySum(int[] nums, int k) {
+		int n = nums.length, sum, j;
+
+		for(int i = 0; i < n - 1; i ++) {
+			sum = nums[i];
+			j = i + 1;
+			while(j < n) {
+				sum += nums[j ++];
+				if(k == 0 && sum == 0
+						|| k != 0 && sum % k == 0) return true;  // -4 % 2 == 0
+			}
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(checkSubarraySum(new int[]{23, 2, 4, 6, 7}, 6));
+		System.out.println(checkSubarraySum(new int[]{23, 2, 6, 4, 7}, 6));
+	}
+}
--- java/src/queue/SurroundedRegions130.java
+++ java/src/queue/SurroundedRegions130.java
@@ -1,4 +1,4 @@
-package array;
+package queue;
 
 import java.util.*;
 
@@ -25,14 +25,16 @@ import java.util.*;
  */
 public class SurroundedRegions130 {
 	/**
-	 * 以边界的'O'为起点，深度优先遍历
+	 * way1: solve(),以边界的'O'为起点，广度优先遍历(BFS)
+	 *   solve_Optimize()针对该版本做了代码优化
+	 *
+	 * way2: 以边界的'O'为起点，深度优先遍历(DFS)，未实现
 	 */
 	public static void solve(char[][] board) {
 		int n = board.length;
 		if(n <= 0) return;
 		int m = board[0].length;
-		char[][] result = new char[n][m];
-		List<Location> visited = new ArrayList<>();
+		Set<Location> visited = new HashSet<>();
 
 		Queue<Location> queue = new LinkedList<>();
 		for(int i = 0; i < n; i ++) {  // 第一列和最后一列
@@ -48,7 +50,7 @@ public class SurroundedRegions130 {
 		Location lct1, lct2;
 		while(!queue.isEmpty()) {
 			lct1 = queue.poll();
-			result[lct1.x][lct1.y] = 'O';
+			board[lct1.x][lct1.y] = '1';
 
 			// up
 			if(lct1.x >= 1) {
@@ -85,12 +87,12 @@ public class SurroundedRegions130 {
 
 		for(int i = 0; i < n; i ++) {
 			for(int j = 0; j < m; j ++) {
-				board[i][j] = result[i][j] == 'O' ? 'O' : 'X';
+				board[i][j] = board[i][j] == '1' ? 'O' : 'X';
 			}
 		}
 	}
 
-	private static void addLocation(int x, int y, Queue<Location> queue, List<Location> visited) {
+	private static void addLocation(int x, int y, Queue<Location> queue, Set<Location> visited) {
 		Location lct = new Location(x, y);
 		queue.offer(lct);
 		visited.add(lct);
@@ -110,9 +112,68 @@ public class SurroundedRegions130 {
 		}
 
 		@Override
-		public int hashCode() {  // 使用HashSet判重，每个Location对象的Hash Code都需要相同，退化成list
+		public int hashCode() {  // 使用HashSet判重，每个Location对象的Hash Code都需要相同，退化成链表或红黑树
 			return 1;
 		}
+	}
+
+	/**
+	 * 针对slove()进行代码优化：
+	 *   通过改变加入队列位置的字符来避免重复添加
+	 */
+	public static void solve_Optimize(char[][] board) {
+		int n = board.length;
+		if(n <= 0) return;
+		int m = board[0].length;
+
+		Queue<Location> queue = new LinkedList<>();
+		for(int i = 0; i < n; i ++) {  // 第一列和最后一列
+			if(board[i][0] == 'O') addLocation_Optimize(i, 0, queue, board);
+			if(board[i][m - 1] == 'O') addLocation_Optimize(i, m - 1, queue, board);
+		}
+
+		for(int j = 1; j < m - 1; j ++) {  // 第一行和最后一行除四个角
+			if(board[0][j] == 'O') addLocation_Optimize(0, j, queue, board);
+			if(board[n - 1][j] == 'O') addLocation_Optimize(n - 1, j, queue, board);
+		}
+
+		Location lct1;
+		while(!queue.isEmpty()) {
+			lct1 = queue.poll();
+			board[lct1.x][lct1.y] = '1';
+
+			// up
+			if(lct1.x >= 1 && board[lct1.x - 1][lct1.y] == 'O') {
+				addLocation_Optimize(lct1.x - 1, lct1.y, queue, board);
+			}
+
+			// down
+			if(lct1.x < n - 1 && board[lct1.x + 1][lct1.y] == 'O') {
+				addLocation_Optimize(lct1.x + 1, lct1.y, queue, board);
+			}
+
+			// left
+			if(lct1.y >= 1 && board[lct1.x][lct1.y - 1] == 'O') {
+				addLocation_Optimize(lct1.x, lct1.y - 1, queue, board);
+			}
+
+			// right
+			if(lct1.y < m - 1 && board[lct1.x][lct1.y + 1] == 'O') {
+				addLocation_Optimize(lct1.x, lct1.y + 1, queue, board);
+			}
+		}
+
+		for(int i = 0; i < n; i ++) {
+			for(int j = 0; j < m; j ++) {
+				board[i][j] = board[i][j] == '1' ? 'O' : 'X';
+			}
+		}
+	}
+
+	private static void addLocation_Optimize(int x, int y, Queue<Location> queue, char[][] board) {
+		Location lct = new Location(x, y);
+		queue.offer(lct);
+		board[x][y] = '2';
 	}
 
 	public static void main(String[] args) {
@@ -130,7 +191,8 @@ public class SurroundedRegions130 {
 				{'X','X','O','X','O'}
 		};
 
-		solve(board);
+//		solve(board);
+		solve_Optimize(board);
 		for(int i = 0; i < board.length; i ++) {
 			System.out.println(Arrays.toString(board[i]));
 		}
