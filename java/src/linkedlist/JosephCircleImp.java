dab097e1c5c55915ad08e463b20a0e5a2182fc0c
workspace <13564472805@163.com>
2020-08-05 17:31:31 +0800

    polish josephCircleImp

--- java/src/linkedlist/JosephCircleImp.java
+++ java/src/linkedlist/JosephCircleImp.java
@@ -8,18 +8,18 @@ public class JosephCircleImp {
 
 
     /**
-     * 模拟约瑟夫环思路
-     * 时间复杂度 O(m*n)，复杂度较高
-     * @param n 总数
-     * @param m 跳过的数字
-     * @return 胜利者编号
+     * simulation process
+     *
+     * time complexity: O(m*n)
+     * space complexity: O(1)
      */
-    public static int simulate(int n, int m){
+    public static int simulate(int n, int m) {
+        assert n > 0 && m > 0;
         Node starter = createJosephCircle(n);
 
-        for(int i = 0; i < n - 1; i ++){ // 执行次数，胜者只有一个
+        for (int i = 0; i < n - 1; i++) { // there is only one winner
             Node node = starter;
-            for(int j = 1; j < m - 1; j ++){
+            for (int j = 1; j < m - 1; j++) {
                 node = node.next;
             }
             node.next = node.next.next;
@@ -33,22 +33,20 @@ public class JosephCircleImp {
     /**
      * 数学方法
      * 0 1 2 3 ... m-1 m ... n-1    (n阶序列，m-1出列)
-     * m m+1 ...  n-1 0  ... m-2  (新的 n-1 序列)
-     * 0 1 2  ... n-m-1 ... m'-1 m' ... n-2 (重新编号)
+     *
+     * m m+1 m+2 ...  n-1   0       1    2        ...   m-2                 (新的 n-1 序列)
+     * 0  1   2  ... n-m-1 n-m   n-m+1  n-m+2     ... n-m+(m-2)=(n-2)         (重新编号)
      * f(x,m) 表示问题规模为 x 时的下标编号：f(x,m) = (f(x-1) + m) mod x
      * 且  f(1,m) 为最终的下标序列，只有 1 个结果且为 0
      *
-     * 循环实现：时间复杂度 O(n)
-     * @param n 问题规模
-     * @param m m > 0
-     * @return 胜利者index
+     * time complexity: O(n)
+     * space complexity: O(1)
      */
-    public static int josephCircle(int n, int m){
-
-        if(m <= 0) return -1;
+    public static int josephCircle(int n, int m) {
+        assert n > 0 && m > 0;
 
         int result = 0;
-        for(int i = 2; i <= n; i ++){
+        for (int i = 2; i <= n; i++) {
             result = (result + m) % i;
         }
 
@@ -60,32 +58,29 @@ public class JosephCircleImp {
      * 递归实现: f(x,m) = (f(x-1) + m) mod x
      * 时间复杂度： O(n)，可以使用递归树方法判断
      */
-    public static int josephCircleByRecursion(int n, int m){
-        if(m < 1) return -1;
-        if(n == 1) return 0;
+    public static int josephCircleByRecursion(int n, int m) {
+        if (m < 1) return -1;
+        if (n == 1) return 0;
 
         return (josephCircleByRecursion(n - 1, m) + m) % n;
     }
 
     /**
      * 创建约瑟夫环，编号从0开始
-     * @param n
-     * @return
      */
-    private static Node createJosephCircle(int n){
-
-        if( n <= 0 ) return null;
+    private static Node createJosephCircle(int n) {
+        assert n > 0;
 
         Node head = new Node(0, null);
-        Node current = head;
+        Node tail = head;
 
-        for(int i = 1; i < n; i ++ ){
-            Node node = new Node(i,null);
-            current.next = node;
-            current = node;
+        for (int i = 1; i < n; i++) {
+            Node node = new Node(i, null);
+            tail.next = node;
+            tail = node;
         }
 
-        current.next = head;
+        tail.next = head;
 
         return head;
     }
