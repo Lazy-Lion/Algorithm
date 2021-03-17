fd829b5b399c57853441fd1f9e503055248977ac
workspace <13564472805@163.com>
2021-03-17 17:34:30 +0800

    polish leetcode package

--- java/src/leetcode/SearchInRotatedSortedArray.java
+++ java/src/leetcode/SearchInRotatedSortedArray.java
@@ -35,13 +35,15 @@ public class SearchInRotatedSortedArray {
             }
 
             // 循环数组，划分之后至少有半边是单调递增的
-            if (nums[left] <= nums[middle]) { // left ascending
+            if (nums[left] <= nums[middle]) {
+                // left ascending
                 if (nums[middle] > target && nums[left] <= target) {
                     right = middle - 1;
                 } else {
                     left = middle + 1;
                 }
-            } else { // right ascending
+            } else {
+                // right ascending
                 if (nums[middle] < target && nums[right] >= target) {
                     left = middle + 1;
                 } else {
@@ -51,5 +53,43 @@ public class SearchInRotatedSortedArray {
         }
 
         return -1;
+    }
+
+
+    public static int search_2(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int middle = left + ((right - left) >> 1);
+            if (nums[middle] == target) {
+                return middle;
+            }
+
+            if (nums[middle] < nums[nums.length - 1]) {
+                // 右侧升序
+                if (target <= nums[right] && target >= nums[middle]) {
+                    // 从右侧查询
+                    left = middle + 1;
+                } else {
+                    // 从左侧查询
+                    right = middle - 1;
+                }
+            } else {
+                // 左侧升序
+                if (target >= nums[left] && target <= nums[middle]) {
+                    // 从左侧查询
+                    right = middle - 1;
+                } else {
+                    // 从右侧查询
+                    left = middle + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(search_2(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
+        System.out.println(search_2(new int[]{1,3}, 3));
     }
 }
